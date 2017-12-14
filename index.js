55ab987806d5781884b4968bb5a10e316812007c
Quentin <q.smith1@yahoo.com>
2017-12-14 16:27:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,20 +6,32 @@ function takeANumber(katzDeliLine, newCustomer) {
   if (katzDeliLine.length === 1) {
     return "Welcome, " + newCustomer + ". You are number " + [i+1] + " in line."
   }
+<<<<<<< HEAD
   else if (katzDeliLine.length > 1) {
+=======
+  else if (katzDeliLine.length > 0) {
+>>>>>>> 454d9b4e2dedee901cc658ab9c782359673089a5
         return "Welcome, " + newCustomer + ". You are number " + (newCustomer.length-1) + " in line."
   }
 }
 
 function nowServing(katzDeliLine) {
   var currentCustomer = katzDeliLine.shift()
+<<<<<<< HEAD
   if(katzDeliLine.length > 1) {
+=======
+  if(katzDeliLine.length > 0){
+>>>>>>> 454d9b4e2dedee901cc658ab9c782359673089a5
      return 'Currently serving ' + currentCustomer +'.' ;
 }    else if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   }
 }
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 454d9b4e2dedee901cc658ab9c782359673089a5
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
